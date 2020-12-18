978a44e85df0faedc2a23f3557ae3930ae80a3dc
JacobSmolii <smolii.vas@gmail.com>
2020-12-18 12:21:40 -0800

    container

---
+++ container_with_water.js
@@ -0,0 +1,24 @@
+// 11. Container With Most Water
+
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+// n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0).
+// Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+// Notice that you may not slant the container.
+
+var maxArea = function (height) {
+	let left = 0;
+	let right = height.length - 1;
+	let maxarea = 0;
+	while (left < right) {
+		maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * (right - left));
+		if (height[left] < height[right])
+			left++;
+		else
+			right--;
+	}
+	return maxarea;
+};
+
+let arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+console.log(maxArea(arr));
